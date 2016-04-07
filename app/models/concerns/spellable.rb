c471a13648ce62b5e6f729d3aeb3ed743d4460ac
John Bedworth <jgbedworth@gmail.com>
2016-04-07 13:48:35 -0700

    add logic for loading dictionary from web, start on checking logic

--- app/models/concerns/spellable.rb
+++ app/models/concerns/spellable.rb
@@ -22,14 +22,33 @@ class Spellable
     # Rule 3 - words that match words in our dictionary but are missing one or more vowels
   end
 
+  def equal?( string )
+    _word = Spellable.new( string )
+    self.word == _word.word
+  end
+
+  def only_differs_in_repeating?( string )
+    _word = Spellable.new( string )
+    unless self.word == _word.word
+
+      self.word.squeeze != _word.squeeze
+    else
+      true
+    end
+    _sword = self.word.squeeze
+
+  end
+
+  # TODO: Find out if I should treat 'y' as a vowel or not for spelling
   def self.remove_vowels(string)
     string.gsub(/[aeiou]/, '')
   end
 
 private
 
+  # TODO: Find out if ' is the only special character I shouldn't strip
   def self.clean_up(string)
-    string.gsub(/[^a-z]/i, '')
+    string.gsub(/[^a-z']/i, '')
   end
 
 end
--- app/models/dictionary_word.rb
+++ app/models/dictionary_word.rb
@@ -1,12 +1,36 @@
+require 'rest-client'
+
 class DictionaryWord < ApplicationRecord
 
   default_scope -> { order(:word )}
   validates :word, presence: true, uniqueness: true
   before_save :_before_save
 
+  def self.find_word( string )
+    _lookup = Spellable.new( string )
+    DictionaryWord.where( :word => _lookup.word )
+  end
+
   def self.find_suggestions( string )
     _lookup = Spellable.new( string )
     DictionaryWord.where( :hash_string => _lookup.word_hash )
+  end
+
+  def self.load
+    response = RestClient.get 'http://tinyurl.com/bvapsn7'
+    if response.code == 200
+      DictionaryWord.delete_all
+      dictionary = response.body
+      dictionary.each_line do |word|
+        begin
+          DictionaryWord.create(:word => word)
+        rescue ActiveRecord::RecordNotUnique => e
+          #could capture this dupe in a second table, but just skip it and move on...
+          puts e.message
+          puts "Duplicate Record Error (#{word} all ready exists in dictionary), non catastrophic. Continuing onward..."
+        end
+      end
+    end
   end
 
 private
